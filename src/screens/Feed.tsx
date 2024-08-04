23894aaf425849ad57296aad5376b91bdb91d462
Gabrielle Oliveira <oliveiragabrielle662@gmail.com>
2024-08-04 11:46:26 -0300

    feat: loading data warning

--- src/screens/Feed.tsx
+++ src/screens/Feed.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, FlatList, Image, StyleSheet } from 'react-native';
+import { View, Text, FlatList, Image, StyleSheet, ActivityIndicator } from 'react-native';
 import { api } from '../lib/axios';
 
 interface Competitor {
@@ -19,6 +19,7 @@ interface Event {
 const EventsList: React.FC = () => {
   const [events, setEvents] = useState<Event[]>([]);
   const [error, setError] = useState<string | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(true);
 
   useEffect(() => {
     api.get('/events')
@@ -28,19 +29,36 @@ const EventsList: React.FC = () => {
       .catch(error => {
         setError(error.message);
         console.error('Error fetching events:', error);
-      });
+      })
+      .finally(() => {
+        setIsLoading(false);
+      })
   }, []);
 
-  if (error) {
-    return <View><Text>Error: {error}</Text></View>;
+  if (isLoading) {
+    return (
+      <View style={styles.loadingView}>
+        <Text style={styles.loadingText}>
+          Carregando eventos...
+        </Text>
+        <ActivityIndicator size="large" color="#f0b13f" />
+      </View>
+    )
   }
 
   return (
-    <FlatList
-      data={events}
-      keyExtractor={item => item.id.toString()}
-      renderItem={({ item }) => <EventCard event={item} />}
-    />
+    <View>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>
+          Jogos Olímpicos Paris 2024
+        </Text>
+      </View>
+      <FlatList
+        data={events}
+        keyExtractor={item => item.id.toString()}
+        renderItem={({ item }) => <EventCard event={item} />}
+      />
+    </View>
   );
 };
 
@@ -68,6 +86,17 @@ const EventCard: React.FC<EventCardProps> = ({ event }) => {
 };
 
 const styles = StyleSheet.create({
+  header: {
+    backgroundColor: "#e8354b",
+    padding: 30,
+  },
+  headerTitle: {
+    color: '#fff',
+    fontWeight: "bold",
+    fontSize: 18,
+    textAlign: "center",
+    alignItems: "center",
+  },
   card: {
     borderWidth: 1,
     borderColor: '#ddd',
@@ -93,6 +122,17 @@ const styles = StyleSheet.create({
     width: 20,
     height: 20,
     marginRight: 10,
+  },
+  loadingView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+
+  },
+  loadingText: {
+    fontWeight: "bold",
+    marginBottom: 20,
+    fontSize: 18,
   },
 });
 
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -39,7 +39,8 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: "center",
         alignItems: "center",
-        backgroundColor: "#fff",
+        backgroundColor: "#007dbf",
+        opacity: 0.7,
         padding: 20,
     },
     title: {
